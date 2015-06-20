158472d881511bf59df30c848b5c7d4737be2622
EijiHsu <apolkingg8@gmail.com>
2015-06-21 01:09:51 +0800

    update

--- Roman to Integer.js
+++ Roman to Integer.js
@@ -3,40 +3,79 @@
 
  Input is guaranteed to be within the range from 1 to 3999.
 */
-
+'use strict'
 /**
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    //FIXME: bad solution. 400ms
-    var MARKS = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000,
+/*var romanToInt = function(s) {
+    //fucking bad performance with map... 536ms
+    var MARKS = new Map([
+        ['I', 1],
+        ['V', 5],
+        ['X', 10],
+        ['L', 50],
+        ['C', 100],
+        ['D', 500],
+        ['M', 1000],
 
-        IV: 4,
-        IX: 9,
-        XL: 40,
-        XC: 90,
-        CD: 400,
-        CM: 900
-    };
+        ['IV', 4],
+        ['IX', 9],
+        ['XL', 40],
+        ['XC', 90],
+        ['CD', 400],
+        ['CM', 900]
+    ]);
 
     var res = 0;
+    var n = s.length;
 
-    for(var i=0; i< s.length; i++) {
+    for(var i=0; i< n; i++) {
         var rom1 = s[i],
             rom2 = s[i+1];
 
-        if(MARKS[rom1+rom2] === undefined) {
+        if(!MARKS.has(rom1+rom2)) {
+            res += MARKS.get(rom1);
+        } else {
+            res += MARKS.get(rom1+rom2);
+            i++;
+        }
+    }
+
+    return res
+};*/
+'use strict'
+const MARKS = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+
+    IV: 4,
+    IX: 9,
+    XL: 40,
+    XC: 90,
+    CD: 400,
+    CM: 900
+};
+
+var romanToInt = function(s) {
+    //FIXME: bad solution. 400ms
+
+    var res = 0;
+    const n = s.length;
+
+    for(var i=0; i<n; i++) {
+        const rom1 = s[i],
+            rom2 = rom1 + s[i+1];
+
+        if(MARKS[rom2] === undefined) {
             res += MARKS[rom1];
         } else {
-            res += MARKS[rom1+rom2];
+            res += MARKS[rom2];
             i++;
         }
     }
@@ -45,4 +84,4 @@ var romanToInt = function(s) {
 };
 
 console.log(romanToInt('XIX')); //19
-console.log(romanToInt('DCCCXC')); //890
+console.log(romanToInt("DCXXI")); //621
