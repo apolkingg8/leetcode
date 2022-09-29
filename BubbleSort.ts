ff2de6bb546987403511dcf164b2b33bd8bc6bec
apolkingg8 <apolkingg8@gmail.com>
2022-09-29 14:46:24 +0800

    :sparkles: More

--- BubbleSort.ts
+++ BubbleSort.ts
@@ -5,9 +5,7 @@ const bubbleSort = (arr: number[])=> {
     for(let i=0; i<arr.length; i++) {
         for(let j=0; j<arr.length-1; j++) {
             if(res[j] > res[j+1]) {
-                let temp = res[j]
-                res[j] = res[j+1]
-                res[j+1] = temp
+                [res[j], res[j+1]] = [res[j+1], res[j]]
             }
         }
     }
